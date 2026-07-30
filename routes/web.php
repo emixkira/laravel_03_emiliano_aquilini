407f461205909be0c2027b4df60e133969dbd23a
Emiliano <emix.aqui@gmail.com>
2026-07-30 13:54:02 +0200

    Correzione esercizio Laravel 03

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,10 @@
-
 <?php
 
-use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/', [ArticleController::class, 'home'])
-    ->name('home');
+Route::get('/', [ArticleController::class, 'home'])->name('home');
 
-Route::get('/articoli', [ArticleController::class, 'index'])
-    ->name('articles.index');
+Route::get('/articoli', [ArticleController::class, 'index'])->name('articles.index');
 
-Route::get('/articoli/{id}', [ArticleController::class, 'show'])
-    ->name('articles.show');
+Route::get('/articoli/{id}', [ArticleController::class, 'show'])->name('articles.show');
